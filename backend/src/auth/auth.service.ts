3752a19a77eb273f710862665a430d8f8bafa112
rachel shindelus <rachelfshindelus@gmail.com>
2025-10-19 12:33:40 -0700

    Implemented login form and session expiration timer

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,18 +1,17 @@
 import { Injectable } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt'; // needed for JSON tokens
+import { JwtService } from '@nestjs/jwt';       // need for JSON tokens (expiration)
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly jwtService: JwtService) {} 
-
-  async login(username: string, password: string) {
-    // Temporary fake logic
-    if (username === 'admin' && password === '1234') {
-      const payload = { username };
-      const token = this.jwtService.sign(payload, { expiresIn: '20m' }); 
-      return { message: 'Login successful', user: { username }, token };
-    } else {
-      return { message: 'Invalid credentials' };
+    async login(username: string, password: string) {
+        // Temporary fake logic
+        if (username === 'admin' && password === '1234') {
+            const payload = { username };
+            const token = this.jwtService.sign(payload, { expiresIn: '20m' });      // token will expire in 20 minutes
+            return { message: 'Login successful', user: { username } };
+        } else {
+            return { message: 'Invalid credentials' };
+        }
     }
   }
 }
