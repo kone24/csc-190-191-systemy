a98f1443b7c8fe30257cf152c28ec959416604b4
shing <strinh2@csus.edu>
2025-10-28 20:54:21 -0700

    Applied changes to middleware and ProtectedLayout per Tuan's comments.

--- frontend/src/app/middleware.tsx
+++ frontend/src/app/middleware.tsx
@@ -9,12 +9,11 @@ export const config = {
 // Redirect to login if no session cookie is present
 export function middleware(request: NextRequest) {
   const url = request.nextUrl.clone();
-  if (request.cookies.has('session')) {
-    return NextResponse.next();
-
+  if (!request.cookies.has('session')) {
     const url = request.nextUrl.clone();
     url.pathname = '/login';
-    url.SearchParams.set('next', request.nextUrl.pathname + request.nextUrl.search);
+    url.searchParams.set('next', request.nextUrl.pathname + request.nextUrl.search);
     return NextResponse.redirect(url);
   }
+  return NextResponse.next()
 }
--- frontend/src/app/protectedaLayout.tsx
+++ frontend/src/app/protectedaLayout.tsx
@@ -3,7 +3,7 @@ import {cookies} from "next/headers";
 import { redirect } from "next/navigation";
 
 // Block rendering if no session cookie is present
-export default function ProtectedLayout({children}: {children: ReactNode}) {
-    if (!cookies().has('session')) redirect('/login');
+export default async function ProtectedLayout({children}: {children: ReactNode}) {
+    if (!(await cookies()).has('session')) redirect('/login');
     return <>{children}</>;
 }
